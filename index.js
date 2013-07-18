6142e6c7390475cc1dc764ae9fdf59815bf4e8ea
zeke <zeke@sikelianos.com>
2013-07-18 00:02:18 -0700

    support devDependencies too

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 appAPI.ready(function($) {
-    //console.log(Date());
+  //console.log(Date());
 
-    // Are we on github.com?
+  // Are we on github.com?
   if (!document.location.href.match(/github\.com/)) return;
 
   // Are we on a repo page?
@@ -15,65 +15,108 @@ appAPI.ready(function($) {
   var user = pathParts[1]
   var repo = pathParts[2]
   var pkgUrl = "http://github-raw-cors-proxy.herokuapp.com/" + user + "/" + repo + "/blob/master/package.json"
-  console.log("pkgUrl", pkgUrl)
 
-  // Add a heading to the list of NPM modules
+  // Set up list containers and headings
   $('#js-repo-pjax-container').append("<h2>npm dependencies</h2>")
+  $('#js-repo-pjax-container').append("<ol id='deps' class='deps'></ol>")
+  $('#js-repo-pjax-container').append("<h2>npm devDependencies</h2>")
+  $('#js-repo-pjax-container').append("<ol id='devDeps' class='deps'></ol>")
 
-  // Set up DOM container for list of modules
-  $('#js-repo-pjax-container').append("<ol id='pkgs'></ol>")
-  var pkgList = $("#pkgs")
+  var depList = $("#deps")
+  var devDepList = $("#devDeps")
+
+
+  function applyStyles() {
+
+    $('.deps').css({
+      listStyle: 'none',
+      margin: '10px 0 10px 0'
+    });
+
+    $('.deps > li').css({
+      padding: '10px 0',
+      borderBottom: "1px solid #DDD"
+    });
+
+    $('.deps > li:last').css({
+      borderBottom: "none"
+    });
+
+    $('.deps > li.empty').css({
+      opacity: '0.5'
+    });
+
+    $('.deps > li > span').css({
+      display: "inline-block"
+    });
+
+    $('.deps > li > span.name').css({
+      minWidth: "500px",
+      maxWidth: "600px"
+    });
+
+    $('.deps > li > span.count').css({
+      color: "#999",
+      minWidth: "100px"
+    });
+
+    $('.deps > li > span.count em').css({
+      color: "#000",
+      fontStyle: "normal"
+    });
+
+  }
+
 
   appAPI.request.get(pkgUrl, function(data) {
     var pkg = JSON.parse(data);
 
-      if (Object.keys(pkg.dependencies).length === 0) {
-      pkgList.append("<li>No dependencies found in this repo's package.json</li>")
-      return;
-      }
+    // Dependencies
+    // -------------------------------------------------------------------------------
 
-      var depNames = Object.keys(pkg.dependencies);
+    if (pkg.dependencies === undefined) {
+      depList.append("<li class='empty'>No dependencies found in package.json</li>")
+      return;
+    }
+
+    var depNames = Object.keys(pkg.dependencies);
 
     for (var i in depNames) {
       var name = depNames[i];
       var depUrl = "http://npm-registry-cors-proxy.herokuapp.com/" + name
 
-      pkgList.append("<li id='dep-" + name + "'><a href='https://npmjs.org/package/" + name + "'>" + name + "</a>&nbsp;&nbsp;</li>");
+      depList.append("<li id='dep-" + name + "'><a href='https://npmjs.org/package/" + name + "'>" + name + "</a>&nbsp;&nbsp;</li>");
 
       appAPI.request.get(depUrl, function(data) {
         var dep = JSON.parse(data);
         $("#dep-"+dep.name).append(dep.description)
+        applyStyles()
       });
     }
 
-    $('#pkgs').css({
-      listStyle: 'none',
-      margin: '10px 0 10px 0'
-    });
+    // Dev Dependencies
+    // -------------------------------------------------------------------------------
 
-    $('#pkgs > li').css({
-      padding: '10px 0',
-      borderBottom: "1px solid #DDD"
-    });
+    if (pkg.devDependencies === undefined) {
+      devDepList.append("<li class='empty'>No devDependencies found in package.json</li>")
+      return;
+    }
 
-    $('#pkgs > li > span').css({
-      display: "inline-block"
-    });
+    var depNames = Object.keys(pkg.devDependencies);
 
-    $('#pkgs > li > span.name').css({
-      minWidth: "500px",
-      maxWidth: "600px"
-    });
+    for (var i in depNames) {
+      var name = depNames[i];
+      var depUrl = "http://npm-registry-cors-proxy.herokuapp.com/" + name
+      console.log(depUrl)
 
-    $('#pkgs > li > span.count').css({
-      color: "#999",
-      minWidth: "100px"
-    });
+      devDepList.append("<li id='devDep-" + name + "'><a href='https://npmjs.org/package/" + name + "'>" + name + "</a>&nbsp;&nbsp;</li>");
 
-    $('#pkgs > li > span.count em').css({
-      color: "#000",
-      fontStyle: "normal"
-    });
+      appAPI.request.get(depUrl, function(data) {
+        var dep = JSON.parse(data);
+        $("#devDep-"+dep.name).append(dep.description)
+        applyStyles()
+      });
+    }
 
   });
 
