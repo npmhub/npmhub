26947fb278705bcb8c5c100d8ef58297a335b38c
Zeke Sikelianos <zeke@sikelianos.com>
2016-02-14 21:02:48 -0800

    add post-install page

---
+++ INSTALLED.md
@@ -0,0 +1,3 @@
+# Thanks for installing npm-hub!
+
+There's nothing to configure. Just surf around on GitHub and whenever you're viewing a repo with npm dependencies, you'll see them listed below the README. For a good example, check out browserify's dependency list: https://github.com/substack/node-browserify#dependencies
--- index.js
+++ index.js
@@ -1,124 +1,129 @@
-appAPI.ready(function($) {
-  //console.log(Date());
-
+appAPI.ready(function ($) {
   // Are we on github.com?
-  if (!document.location.href.match(/github\.com/)) return;
+  if (!document.location.href.match(/github\.com/)) return
 
   // Are we on a repo page?
   var pathParts = document.location.pathname.match(/\/+([^/]*)\/([^(/|\?)]*)/)
-  if (!pathParts) return;
+  if (!pathParts) return
 
   // Does the repo have a package.json?
-  if (!$("table.files").text().match(/package\.json/)) return;
+  if (!$('table.files').text().match(/package\.json/)) return
 
   // Assemble API URL for fetching raw json from github
   var user = pathParts[1]
   var repo = pathParts[2]
-  var pkgUrl = "http://github-raw-cors-proxy.herokuapp.com/" + user + "/" + repo + "/blob/master/package.json"
+  var pkgUrl = 'http://github-raw-cors-proxy.herokuapp.com/' + user + '/' + repo + '/blob/master/package.json'
 
   // Set up list containers and headings
-  $('#js-repo-pjax-container').append("<h2>npm dependencies</h2>")
-  $('#js-repo-pjax-container').append("<ol id='deps' class='deps'></ol>")
-  $('#js-repo-pjax-container').append("<h2>npm devDependencies</h2>")
-  $('#js-repo-pjax-container').append("<ol id='devDeps' class='deps'></ol>")
+  $('#readme').append("<h2 id='dependencies' class='npm-hub-heading'>Dependencies</h2>")
+  $('#readme').append("<ol id='depsList' class='deps'></ol>")
+  $('#readme').append("<h2 id='dev-dependencies' class='npm-hub-heading'>Dev Dependencies</h2>")
+  $('#readme').append("<ol id='devDepsList' class='deps'></ol>")
 
-  var depList = $("#deps")
-  var devDepList = $("#devDeps")
+  var $depsList = $('#depsList')
+  var $devDepsList = $('#devDepsList')
 
-
-  function applyStyles() {
-
+  function applyStyles () {
     $('.deps').css({
       listStyle: 'none',
       margin: '10px 0 10px 0'
-    });
+    })
 
     $('.deps > li').css({
       padding: '10px 0',
-      borderBottom: "1px solid #DDD"
-    });
+      borderBottom: '1px solid #DDD'
+    })
 
     $('.deps > li:last').css({
-      borderBottom: "none"
-    });
-
-    $('.deps > li.empty').css({
-      opacity: '0.5'
-    });
+      borderBottom: 'none'
+    })
 
     $('.deps > li > span').css({
-      display: "inline-block"
-    });
+      display: 'inline-block'
+    })
 
     $('.deps > li > span.name').css({
-      minWidth: "500px",
-      maxWidth: "600px"
-    });
+      minWidth: '500px',
+      maxWidth: '600px'
+    })
 
     $('.deps > li > span.count').css({
-      color: "#999",
-      minWidth: "100px"
-    });
+      color: '#999',
+      minWidth: '100px'
+    })
 
     $('.deps > li > span.count em').css({
-      color: "#000",
-      fontStyle: "normal"
-    });
+      color: '#000',
+      fontStyle: 'normal'
+    })
+
+    $('.npm-hub-heading').css({
+      paddingTop: '20px'
+    })
+
+    $('li.empty').css({
+      opacity: '0.6'
+    })
 
   }
 
-
-  appAPI.request.get(pkgUrl, function(data) {
-    var pkg = JSON.parse(data);
+  appAPI.request.get(pkgUrl, function (data) {
+    var pkg = JSON.parse(data)
 
     // Dependencies
     // -------------------------------------------------------------------------------
 
     if (pkg.dependencies === undefined) {
-      depList.append("<li class='empty'>No dependencies found in package.json</li>")
-      return;
+      $depsList.append("<li class='empty'>None found in package.json</li>")
+      applyStyles()
+    } else {
+      var depNames = Object.keys(pkg.dependencies)
+
+      for (var i in depNames) {
+        var name = depNames[i]
+        var depUrl = 'http://npm-registry-cors-proxy.herokuapp.com/' + name
+
+        $depsList.append("<li id='dep-" + name + "'><a href='https://npmjs.org/package/" + name + "'>" + name + '</a>&nbsp;&nbsp;</li>')
+
+        appAPI.request.get(depUrl, function (data) {
+          var dep = JSON.parse(data)
+          $('#dep-' + dep.name).append(dep.description)
+
+          if (dep.repository && dep.repository.url) {
+            $('#dep-' + dep.name).append(" <a href='" + dep.repository.url + "'>(repo)</a>")
+          }
+          applyStyles()
+        })
+      }
     }
 
-    var depNames = Object.keys(pkg.dependencies);
-
-    for (var i in depNames) {
-      var name = depNames[i];
-      var depUrl = "http://npm-registry-cors-proxy.herokuapp.com/" + name
-
-      depList.append("<li id='dep-" + name + "'><a href='https://npmjs.org/package/" + name + "'>" + name + "</a>&nbsp;&nbsp;</li>");
-
-      appAPI.request.get(depUrl, function(data) {
-        var dep = JSON.parse(data);
-        $("#dep-"+dep.name).append(dep.description)
-        applyStyles()
-      });
-    }
 
     // Dev Dependencies
     // -------------------------------------------------------------------------------
 
     if (pkg.devDependencies === undefined) {
-      devDepList.append("<li class='empty'>No devDependencies found in package.json</li>")
-      return;
+      $devDepsList.append("<li class='empty'>None found in in package.json</li>")
+      applyStyles()
+    } else {
+      var depNames = Object.keys(pkg.devDependencies)
+
+      for (var i in depNames) {
+        var name = depNames[i]
+        var depUrl = 'http://npm-registry-cors-proxy.herokuapp.com/' + name
+
+        $devDepsList.append("<li id='devDep-" + name + "'><a href='https://npmjs.org/package/" + name + "'>" + name + '</a>&nbsp;&nbsp;</li>')
+
+        appAPI.request.get(depUrl, function (data) {
+          var dep = JSON.parse(data)
+          $('#devDep-' + dep.name).append(dep.description)
+          if (dep.repository && dep.repository.url) {
+            $('#devDep-' + dep.name).append(" <a href='" + dep.repository.url + "'>(repo)</a>")
+          }
+          applyStyles()
+        })
+      }
     }
 
-    var depNames = Object.keys(pkg.devDependencies);
+  })
 
-    for (var i in depNames) {
-      var name = depNames[i];
-      var depUrl = "http://npm-registry-cors-proxy.herokuapp.com/" + name
-      console.log(depUrl)
-
-      devDepList.append("<li id='devDep-" + name + "'><a href='https://npmjs.org/package/" + name + "'>" + name + "</a>&nbsp;&nbsp;</li>");
-
-      appAPI.request.get(depUrl, function(data) {
-        var dep = JSON.parse(data);
-        $("#devDep-"+dep.name).append(dep.description)
-        applyStyles()
-      });
-    }
-
-  });
-
-
-});
+})
