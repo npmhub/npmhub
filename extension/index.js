37a8b0087df74d8a4b3505cbde7d5de05b573f1b
Federico Brigante <bfred-it@users.noreply.github.com>
2016-06-25 12:54:36 +0700

    Minor ES6 changes

--- extension/index.js
+++ extension/index.js
@@ -1,21 +1,19 @@
-$(function () {
+$(() => {
   // Are we on a repo page?
-  var pathParts = document.location.pathname.match(/\/+([^/]*)\/([^(/|\?)]*)/)
-  if (!pathParts) return
+  const [, user, repo] = document.location.pathname.match(/\/+([^/]*)\/([^(/|\?)]*)/)
+  if (!user) return
 
   // Does the repo have a package.json?
   if (!$('.files [title="package.json"]').length) return
 
   // Assemble API URL for fetching raw json from github
-  var user = pathParts[1]
-  var repo = pathParts[2]
-  var pkgUrl = 'https://github-raw-cors-proxy.herokuapp.com/' + user + '/' + repo + '/blob/master/package.json'
+  const pkgUrl = 'https://github-raw-cors-proxy.herokuapp.com/' + user + '/' + repo + '/blob/master/package.json'
 
   // Set up list containers and headings
-  var $template = $('#readme').clone().empty().removeAttr('id');
+  const $template = $('#readme').clone().empty().removeAttr('id');
 
-  var $depsList = $("<ol class='deps markdown-body'>");
-  var $devDepsList = $("<ol class='deps markdown-body'>");
+  const $depsList = $("<ol class='deps markdown-body'>");
+  const $devDepsList = $("<ol class='deps markdown-body'>");
 
   $template.clone()
   .append('<h3 id="dependencies">Dependencies', $depsList)
@@ -25,22 +23,20 @@ $(function () {
   .append('<h3 id="dev-dependencies">Dev dependencies', $devDepsList)
   .appendTo('.repository-content');
 
-  fetch(pkgUrl).then(response => response.json()).then(function (pkg) {
+  fetch(pkgUrl).then(response => response.json()).then(pkg => {
     addDependencies(pkg.dependencies, $depsList);
     addDependencies(pkg.devDependencies, $devDepsList);
   });
 
   function addDependencies(dependencies, $list) {
     if (dependencies) {
-      var depNames = Object.keys(dependencies).forEach(name => {
-        console.log(name)
-        var depUrl = 'https://npm-registry-cors-proxy.herokuapp.com/' + name
+      const depNames = Object.keys(dependencies).forEach(name => {
+        const depUrl = 'https://npm-registry-cors-proxy.herokuapp.com/' + name
 
-        var $dep = $("<li><a href='https://npmjs.org/package/" + name + "'>" + name + '</a>&nbsp;&nbsp;</li>')
+        const $dep = $("<li><a href='https://npmjs.org/package/" + name + "'>" + name + '</a>&nbsp;&nbsp;</li>')
         $dep.appendTo($list);
 
-        fetch(depUrl).then(response => response.json()).then(function (dep) {
-          console.log(dep)
+        fetch(depUrl).then(response => response.json()).then(dep => {
           $dep.append(dep.description)
 
           if (dep.repository) {
