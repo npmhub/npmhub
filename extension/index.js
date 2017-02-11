7eb5733d7271f7b1faaa98b9687a9cc40410d384
MatthieuLemoine <ml.94230@gmail.com>
2017-02-11 11:13:18 +0100

    :sparkles: GitLab support

--- extension/index.js
+++ extension/index.js
@@ -1,27 +1,32 @@
+// Are we on a GitLab instance ?
+const isGitLab = !!document.querySelector('.navbar-gitlab');
 // Are we on a repo page that has a package.json?
-const packageLink = document.querySelector('.files [title="package.json"]');
+const packageLink = document.querySelector('.files [title="package.json"], .tree-item-file-name [title="package.json"]');
 
 if (packageLink) {
   const pkgUrl = packageLink.href;
 
   // Set up list containers and headings
-  const $template = $('#readme').clone().empty().removeAttr('id');
+  const $template = $('#readme, .readme-holder').clone().empty().removeAttr('id');
 
   const $depsList = $("<ol class='deps markdown-body'>");
   const $devDepsList = $("<ol class='deps markdown-body'>");
   const $depsVisBtn = $("<button class='btn btn-sm viz-btn' style='float: right; margin: 5px 5px 0 0;' type='button'>Dependency tree visualization");
 
+  const dependenciesHeader    = isGitLab ? '<div id="dependencies" class="file-title"><strong>Dependencies'         : '<h3 id="dependencies">Dependencies';
+  const devDependenciesHeader = isGitLab ? '<div id="dev-dependencies" class="file-title"><strong>Dev dependencies' : '<h3 id="dev-dependencies">Dev dependencies';
+
   $template.clone()
   .append($depsVisBtn)
-  .append('<h3 id="dependencies">Dependencies', $depsList)
-  .appendTo('.repository-content');
+  .append(dependenciesHeader, $depsList)
+  .appendTo('.repository-content, .tree-content-holder');
 
   $template.clone()
-  .append('<h3 id="dev-dependencies">Dev dependencies', $devDepsList)
-  .appendTo('.repository-content');
+  .append(devDependenciesHeader, $devDepsList)
+  .appendTo('.repository-content, .tree-content-holder');
 
   fetch(pkgUrl, { credentials: 'include' }).then(res => res.text()).then(domStr => {
-    const pkg = JSON.parse($(domStr).find('.blob-wrapper').text());
+    const pkg = JSON.parse($(domStr).find('.blob-wrapper, .blob-content').text());
     $depsVisBtn.wrap(`<a href="http://npm.anvaka.com/#/view/2d/${pkg.name}"></a>`);
     addDependencies(pkg.dependencies, $depsList);
     addDependencies(pkg.devDependencies, $devDepsList);
