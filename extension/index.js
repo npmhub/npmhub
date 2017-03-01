df7dc3bfb01ccde6267e75e9b7df2327ed4b1a75
Federico Brigante <bfred-it@users.noreply.github.com>
2017-03-01 13:15:19 +0700

    eslint --fix

--- extension/index.js
+++ extension/index.js
@@ -9,7 +9,7 @@ function esc(str) {
 }
 
 // Are we on a GitLab instance ?
-const isGitLab = !!document.querySelector('.navbar-gitlab');
+const isGitLab = document.querySelector('.navbar-gitlab');
 // Are we on a repo page that has a package.json?
 const packageLink = document.querySelector('.files [title="package.json"], .tree-item-file-name [title="package.json"]');
 
@@ -19,12 +19,16 @@ if (packageLink) {
   // Set up list containers and headings
   const $template = $('#readme, .readme-holder').clone().empty().removeAttr('id');
 
-  const $depsList = $("<ol class='deps markdown-body'>");
-  const $devDepsList = $("<ol class='deps markdown-body'>");
-  const $depsVisBtn = $("<a class='btn btn-sm' style='float: right; margin: 5px'>Dependency tree visualization</a>");
+  const $depsList = $('<ol class="deps markdown-body">');
+  const $devDepsList = $('<ol class="deps markdown-body">');
+  const $depsVisBtn = $('<a class="btn btn-sm" style="float: right; margin: 5px">Dependency tree visualization</a>');
 
-  const dependenciesHeader    = isGitLab ? '<div id="dependencies" class="file-title"><strong>Dependencies'         : '<h3 id="dependencies">Dependencies';
-  const devDependenciesHeader = isGitLab ? '<div id="dev-dependencies" class="file-title"><strong>Dev dependencies' : '<h3 id="dev-dependencies">Dev dependencies';
+  const dependenciesHeader = isGitLab ?
+    '<div id="dependencies" class="file-title"><strong>Dependencies' :
+    '<h3 id="dependencies">Dependencies';
+  const devDependenciesHeader = isGitLab ?
+    '<div id="dev-dependencies" class="file-title"><strong>Dev Dependencies' :
+    '<h3 id="dev-dependencies">Dev Dependencies';
 
   $template.clone()
   .append($depsVisBtn)
@@ -35,7 +39,7 @@ if (packageLink) {
   .append(devDependenciesHeader, $devDepsList)
   .appendTo('.repository-content, .tree-content-holder');
 
-  fetch(pkgUrl, { credentials: 'include' }).then(res => res.text()).then(domStr => {
+  fetch(pkgUrl, {credentials: 'include'}).then(res => res.text()).then(domStr => {
     const pkg = JSON.parse($(domStr).find('.blob-wrapper, .blob-content').text());
     if (pkg.private) {
       $depsVisBtn.remove();
@@ -48,16 +52,16 @@ if (packageLink) {
 
   function addDependencies(dependencies, $list) {
     if (!dependencies || !Object.keys(dependencies).length) {
-      return $list.append("<li class='empty'>No dependencies! 🎉</li>");
+      return $list.append('<li class="empty">No dependencies! 🎉</li>');
     }
 
     Object.keys(dependencies).forEach(name => {
-      const depUrl = 'https://registry.npmjs.org/' + name
-      const $dep = $(`<li><a href='http://ghub.io/${esc(name)}'>${esc(name)}</a>&nbsp;</li>`)
+      const depUrl = 'https://registry.npmjs.org/' + name;
+      const $dep = $(`<li><a href='http://ghub.io/${esc(name)}'>${esc(name)}</a>&nbsp;</li>`);
       $dep.appendTo($list);
       backgroundFetch(depUrl).then(dep => {
-        $dep.append(dep.description)
-      })
+        $dep.append(dep.description);
+      });
     });
   }
 }
