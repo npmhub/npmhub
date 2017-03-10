77f18500edb9e7d7c29584e1c8435359b7d508f1
Federico Brigante <bfred-it@users.noreply.github.com>
2017-03-10 20:12:05 +0700

    Fix anvaka button position /2

--- extension/index.js
+++ extension/index.js
@@ -31,17 +31,17 @@ async function init() {
   const dependencies = Object.keys(pkg.dependencies || {});
   const devDependencies = Object.keys(pkg.devDependencies || {});
 
-  if (dependencies.length && !pkg.private) {
-    const link = html`<a class="npmhub-anvaka btn btn-sm">Dependency tree visualization`;
-    link.href = `http://npm.anvaka.com/#/view/2d/${esc(pkg.name)}`;
-    dependenciesBox.appendChild(link);
-  }
-
   addDependencies(dependenciesBox, dependencies);
 
   // Don't show dev dependencies if there are absolutely no dependencies
   if (dependencies.length || devDependencies.length) {
     addDependencies(createBox('Dev Dependencies'), devDependencies);
+  }
+
+  if (dependencies.length && !pkg.private) {
+    const link = html`<a class="npmhub-anvaka btn btn-sm">Dependency tree visualization`;
+    link.href = `http://npm.anvaka.com/#/view/2d/${esc(pkg.name)}`;
+    dependenciesBox.insertBefore(link, dependenciesBox.firstChild);
   }
 }
 
