2b5ebd9cb98ba69874ee2c44d4b4468a4c33a58d
Federico Brigante <github@bfred.it>
2017-06-19 18:23:25 +0800

    Add options page for custom domain support
    This enables GitHub Enterprise, GitLab Enterprise and GitLab Community
    Edition

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,8 @@ import commonJS from 'rollup-plugin-commonjs';
 
 export default [
   'src/index.js',
-  'src/background.js'
+  'src/background.js',
+  'src/options.js'
 ].map(entry => ({
   entry,
   dest: entry.replace('src', 'extension'),
