6ca90bc21ead20eaddc45de2edf9b2c2594396de
Federico Brigante <github@bfred.it>
2019-03-17 11:57:27 +0800

    Add LRU(1000) cache

--- source/background.js
+++ source/background.js
@@ -5,6 +5,30 @@ import dynamicContentScripts from 'webext-dynamic-content-scripts';
 dynamicContentScripts.addToFutureTabs();
 domainPermissionToggle.addContextMenu();
 
+const cache = new Map();
+const cacheLimit = 1000;
+
+window.npmhubCache = cache;
+
+// Trim cache twice-daily in long browser sessions
+setInterval(() => {
+  for (const name of cache) {
+    if (cache.size < cacheLimit) {
+      return;
+    }
+
+    cache.delete(name);
+  }
+}, 12 * 60 * 60 * 1000);
+
+async function fetchPackageJson(name) {
+  // Scoped repositories contain an escaped slash and a regular at-sign
+  // Example: https://registry.npmjs.org/@shinnn%2Feslint-config-node
+  const url = 'https://registry.npmjs.org/' + name.replace('/', '%2F');
+  const response = await fetch(url);
+  return response.json();
+}
+
 // `background` fetch required to avoid avoid CORB introduced in Chrome 73 https://chromestatus.com/feature/5629709824032768
 chrome.runtime.onMessage.addListener((
   {action, payload},
@@ -14,19 +38,20 @@ chrome.runtime.onMessage.addListener((
   if (action === 'fetch') {
     const {name} = payload;
 
-    // Scoped repositories contain an escaped slash and a regular at-sign
-    // Example: https://registry.npmjs.org/@shinnn%2Feslint-config-node
-    const url = 'https://registry.npmjs.org/' + name.replace('/', '%2F');
+    const promise = cache.get(name) || fetchPackageJson(name);
 
-    fetch(url)
-      .then(response => response.json())
-      .then(json => sendResponse(json))
-      .catch(error => sendResponse({
+    promise.then(sendResponse, error => {
+      cache.delete(name); // Drop errors from cache
+
+      sendResponse({
         error: {
           title: error.title,
           message: error.message
         }
-      }));
+      });
+    });
+
+    cache.set(name, promise);
 
     return true; // Required to signal intent to respond asynchronously
   }
