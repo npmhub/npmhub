1eeb00862226c0c0bcb481ad866bc0e2081c4bb2
Federico Brigante <me@fregante.com>
2020-11-19 23:58:35 -0600

    Meta: Keep Safari version up to date with RGH’s
    Follows https://github.com/sindresorhus/refined-github/commit/b141596aabc36470b16d615ad1bee8f2920d3e06

--- safari/npmhub/ViewController.swift
+++ safari/npmhub/ViewController.swift
@@ -3,7 +3,7 @@ import SafariServices.SFSafariApplication
 import SafariServices.SFSafariExtensionManager
 
 let appName = "npmhub"
-let extensionBundleIdentifier = "org.npmhub.npmhub-Extension"
+let extensionBundleIdentifier = "org.npmhub.npmhub.Extension"
 
 final class ViewController: NSViewController {
 	@IBOutlet var appNameLabel: NSTextField!
