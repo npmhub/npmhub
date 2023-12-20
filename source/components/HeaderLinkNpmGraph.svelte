30d15bba56eb646b15bccf1a9f5e34f2d11ce53a
Federico Brigante <me@fregante.com>
2023-12-20 18:10:45 +0800

    Part 1

---
+++ source/components/HeaderLinkNpmGraph.svelte
@@ -0,0 +1,12 @@
+<script>
+  import HeaderLink from './HeaderLink.svelte';
+  export let packageJson;
+
+  function getInlinePackageUrl({name = 'noname', dependencies, devDependencies, peerDependencies, bundledDependencies}) {
+    const trimmedPackage = {name, dependencies, devDependencies, peerDependencies, bundledDependencies};
+    const params = new URLSearchParams([['packages', JSON.stringify([trimmedPackage])]])
+    return `https://npmgraph.js.org/#${params}`;
+  }
+</script>
+
+<HeaderLink href={getInlinePackageUrl(packageJson)} label="Dependency tree"/>
