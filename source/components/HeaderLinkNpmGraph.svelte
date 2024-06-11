e13849351ecd467e46ae9d70d5c48ace4a1353d3
Federico <me@fregante.com>
2024-06-11 13:41:52 +0800

    Rename package

--- source/components/HeaderLinkNpmGraph.svelte
+++ source/components/HeaderLinkNpmGraph.svelte
@@ -2,7 +2,7 @@
   import HeaderLink from './HeaderLink.svelte';
   export let packageJson;
 
-  function getInlinePackageUrl({name = 'noname', dependencies, devDependencies, peerDependencies, bundledDependencies}) {
+  function getInlinePackageUrl({name = 'uploaded via npmhub', dependencies, devDependencies, peerDependencies, bundledDependencies}) {
     const trimmedPackage = {name, dependencies, devDependencies, peerDependencies, bundledDependencies};
     const parameters = new URLSearchParams([['packages', JSON.stringify([trimmedPackage])]]);
     return `https://npmgraph.js.org/?q=${name}#${parameters}`;
